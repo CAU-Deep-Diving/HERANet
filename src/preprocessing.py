c4a6f8c7ecc61e5dac5d3dd3e73d04ea3bf8cad9
Jsh03 <jsh20030214@gmail.com>
2026-03-17 23:27:47 +0900

    preprocessing revise

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def apply_augmentation(
     inputs,
     lengths,
@@ -15,8 +16,6 @@ def apply_augmentation(
 ):
     """
     epoch 진행률에 따라 augmentation 강도 조절
-    원래 augmentation.txt 로직을 그대로 유지하되
-    notebook에서 바로 실행 가능하도록 포함한 버전
     """
     inputs = inputs.clone()
 
@@ -36,29 +35,21 @@ def apply_augmentation(
         p_shuffle = base_p_shuffle
         p_swap = base_p_swap
 
-    # token dropout
     dropout_mask = (torch.rand_like(inputs.float()) > p_drop).long()
     inputs = inputs * dropout_mask
 
-    # token mask
     mask = (torch.rand_like(inputs.float()) < p_mask) & (inputs != pad_idx)
     inputs[mask] = unk_idx
 
-    # local shuffle / random swap
     for seq in inputs:
         seq_len = (seq != pad_idx).sum().item()
 
         for i in range(seq_len - 1):
             if torch.rand(1).item() < p_shuffle:
-                tmp = seq[i].item()
-                seq[i] = seq[i + 1]
-                seq[i + 1] = tmp
+                seq[i], seq[i + 1] = seq[i + 1], seq[i]
 
         if seq_len > 2 and torch.rand(1).item() < p_swap:
-            idx = torch.randperm(seq_len)[:2]
-            i, j = idx[0].item(), idx[1].item()
-            tmp = seq[i].item()
-            seq[i] = seq[j]
-            seq[j] = tmp
+            i, j = torch.randint(0, seq_len, (2,))
+            seq[i], seq[j] = seq[j], seq[i]
 
     return inputs, lengths, labels
